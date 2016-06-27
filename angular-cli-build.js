c2bb1bb6025511e9a86b82dd8dbbfda7f8f46fce
Carmel Neta <carmelneta@gmail.com>
2016-06-28 00:03:19 +0300

    Initing
    Working scss, material

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -1,21 +1,40 @@
+'use strict';
 // Angular-CLI build configuration
 // This file lists all the node_modules files that will be used in a build
 // Also see https://github.com/angular/angular-cli/wiki/3rd-party-libs
 
 /* global require, module */
 
-var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
+// var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
+const Angular2App = require('angular-cli/lib/broccoli/angular2-app');
+const compileSass = require('broccoli-sass');
+const mergeTrees = require('broccoli-merge-trees');
+const _ = require('lodash');
+const glob = require('glob');
 
 module.exports = function(defaults) {
-  return new Angular2App(defaults, {
-    vendorNpmFiles: [
-      'systemjs/dist/system-polyfills.js',
-      'systemjs/dist/system.src.js',
-      'zone.js/dist/**/*.+(js|js.map)',
-      'es6-shim/es6-shim.js',
-      'reflect-metadata/**/*.+(ts|js|js.map)',
-      'rxjs/**/*.+(js|js.map)',
-      '@angular/**/*.+(js|js.map)'
-    ]
+
+  let sass = mergeTrees(_.map(glob.sync('src/**/*.scss'), function(sassFile) {
+        sassFile = sassFile.replace('src/', '');
+        return compileSass(['src'], sassFile, sassFile.replace(/.scss$/, '.css'));
+    }));
+
+    let appTree = new Angular2App(defaults, {
+        sassCompiler: {
+            includePaths: [
+            'src/style'
+            ]
+        },
+        vendorNpmFiles: [
+        'systemjs/dist/system-polyfills.js',
+        'systemjs/dist/system.src.js',
+        'zone.js/dist/**/*.+(js|js.map)',
+        'es6-shim/es6-shim.js',
+        'reflect-metadata/**/*.+(js|js.map)',
+        'rxjs/**/*.+(js|js.map)',
+        '@angular/**/*.+(js|js.map)',
+        '@angular2-material/**/*'
+        ]
   });
+  return mergeTrees([appTree, sass], { overwrite: true });   
 };
